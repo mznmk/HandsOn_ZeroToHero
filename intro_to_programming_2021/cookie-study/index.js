df9f6cef64f976ecd3230c79ccb7449e59f4a50d
mznmk <mika_develop@mznmk.com>
2022-03-12 06:15:36 +0900

    [N-Prep] finish "cookie-study"

---
+++ intro_to_programming_2021/cookie-study/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// [ import ]
+const http = require('http');
+
+// [ const variable ]
+const port = 8000;
+
+// [ add event to server ]
+const server = http.createServer();
+server.on('request', (req, res) => {
+  const now = Date.now();
+  res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+  res.setHeader('Set-Cookie', `last_access=${now}; expires=Mon, 07 Jan 2036 00:00:00 GMT;`);
+  const last_access_time = now;
+  if (req.headers.cookie) {
+    parseInt(req.headers.cookie.split('last_access=')[1]);
+  }
+  res.end(new Date(last_access_time).toString());
+});
+server.listen(port, () => {
+  console.info(`Listening on ${port}`);
+});
