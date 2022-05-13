d4c97986b4b4e787d1e1f253aaecd7133c92452c
mznmk <mika_develop@mznmk.com>
2022-05-13 22:31:57 +0900

    [NPrep Node] finish "chap4-8 ajax-study"

---
+++ intro_to_programming_2021/chap4/ajax-study/app/entry.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import $ from 'jquery';
+const block = $('#block');
+const scalingButton = $('#scaling-button');
+const movingButton = $('#moving-button');
+const loadavg = $('#loadavg');
+
+scalingButton.click(() => {
+  block.animate({ width: '200pt', height: '200pt' }, 2000);
+  block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+movingButton.click(() => {
+  block.animate({ marginLeft: '500px' }, 500);
+  block.animate({ marginLeft: '20px' }, 1000);
+});
+
+setInterval(() => {
+  $.get('/server-status', {}, (data) => {
+    loadavg.text(data.loadavg.toString());
+   });
+}, 1000);
