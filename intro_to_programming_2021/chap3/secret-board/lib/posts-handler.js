a7106662f0172d5a06cb0854985fa92fda2775a9
mznmk <mika_develop@mznmk.com>
2022-05-23 15:01:22 +0900

    [NNN Node] finish chap3-31 secret-board 9

--- intro_to_programming_2021/chap3/secret-board/lib/posts-handler.js
+++ intro_to_programming_2021/chap3/secret-board/lib/posts-handler.js
@@ -1,6 +1,7 @@
 'use strict'
 
 // [ import library ]
+const crypto = require('crypto');
 const pug = require('pug');
 const Coolies = require('cookies');
 const Post = require('./post');
@@ -14,12 +15,13 @@ dayjs.extend(timezone);
 const util = require('./handler-util');
 const Cookies = require('cookies');
 
+// [ variable ]
 const trackingIdKey = 'tracking_id';
 
 // [ posts handler ]
 function handle(req, res) {
   const cookies = new Cookies(req, res);
-  addTrackingCookie(cookies);
+  const trackingId = addTrackingCookie(cookies, req.user);
 
   switch (req.method) {
     // [ GET ]
@@ -37,7 +39,7 @@ function handle(req, res) {
       });
       console.info(
         `閲覧されました: user: ${req.user}, ` +
-        `trackingID: ${cookies.get(trackingIdKey)}, ` +
+        `trackingId: ${trackingId}, ` +
         `remoteAddress: ${req.socket.remoteAddress} ` +
         `userAgent: ${req.headers['user-agent']} `
       );
@@ -55,7 +57,7 @@ function handle(req, res) {
         console.info('投稿されました: ' + content);
         Post.create({
           content: content,
-          trackingCookie: cookies.get(trackingIdKey),
+          trackingCookie: trackingId,
           postedBy: req.user
         }).then(() => {
           handleRedirectPosts(req, res);
@@ -99,13 +101,40 @@ function handleDelete(req, res) {
       break;
   }
 }
-
-function addTrackingCookie(cookies) {
-  if (!cookies.get(trackingIdKey)) {
-    const trackingId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-    const tomorrow = new Date(Date.now() + (1000 * 60 * 60 * 24));
-    cookies.set(trackingIdKey, trackingId, { expires: tomorrow });
+/**
+ * Cookieに含まれているトラッキングIDに異常がなければその値を返し、
+ * 存在しない場合や異常なものである場合には、再度作成しCookieに付与してその値を返す
+ * @param {Cookies} cookies
+ * @param {String} userName
+ * @return {String} トラッキングID
+ */
+function addTrackingCookie(cookies, userName) {
+  const requestedTrackingId = cookies.get(trackingIdKey);
+  if (isValidTrackingId(requestedTrackingId, userName)) {
+    return (requestedTrackingId);
+  } else {
+    const originalId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    const tommorow = new Date(Date.now() + (1000 * 60 * 60 * 24));
+    const trackingId = originalId + '_' + createValidHash(originalId, userName);
+    cookies.set(trackingIdKey, trackingId, { expires: tommorow });
+    return trackingId;
   }
+}
+
+function isValidTrackingId(trackingId, userName) {
+  if (!trackingId) {
+    return false;
+  }
+  const splitted = trackingId.split('_');
+  const originalId = splitted[0];
+  const requestedHash = splitted[1];
+  return createValidHash(originalId, userName) === requestedHash;
+}
+
+function createValidHash(originalId, userName) {
+  const sha1sum = crypto.createHash('sha1');
+  sha1sum.update(originalId + userName);
+  return sha1sum.digest('hex');
 }
 
 function handleRedirectPosts(req, res) {
--- intro_to_programming_2021/chap3/secret-board/test.js
+++ intro_to_programming_2021/chap3/secret-board/test.js
@@ -4,13 +4,14 @@ const pug = require('pug');
 const assert = require('assert');
 
 // pug のテンプレートにおける xss 脆弱性のテスト
+// ( content 以外は適当な値を設定 )
 const html = pug.renderFile('./views/posts.pug', {
   posts: [
     {
       id: 1,
       content: "<script>alert('test');</script>",
       postedBy: 'guest1',
-      trackingCookie: 1,
+      trackingCookie: "6810017664159257_a79874335941b1fd29195c17a356dd4548da3b1a",
       createdAt: new Date(),
       updatedAt: new Date()
     }
