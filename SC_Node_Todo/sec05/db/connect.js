8b3d48d263d0e2a3ad1e29f9d0a34e58e5804929
mznmk <mika_develop@mznmk.com>
2022-06-29 17:09:08 +0900

    [SC Node Todo] giveup sec05 (fail connect: mongo)

---
+++ SC_Node_Todo/sec05/db/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("now connecting ...");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = connectDB;
